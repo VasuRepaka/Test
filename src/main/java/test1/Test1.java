1c729f852f81fb2f5a9e645a6f8d395097886cd1
jogi <gopal.j31@gmail.com>
2020-09-26 20:45:14 +0530

    Test1 again

--- src/main/java/test1/Test1.java
+++ src/main/java/test1/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	
 	public void m1() 
 	{
-		System.out.println("This message is from Eclipse");
+		System.out.println("This message is from Eclipse again");
 		System.out.println("This message is from GITHUB");
 	}
 
